74af80aacfedbf7c2c7084c15147a9fbfbc9ea84
natedem30 <nathaniel.demchak@holisticai.com>
2024-08-01 13:04:33 -0700

    Updated project encapsulation and fixed __init__.py

--- alignmentbiascheckingtools/__init__.py
+++ alignmentbiascheckingtools/__init__.py
@@ -1,7 +1,7 @@
 from .abcData import abcData
 from .analytics import check_benchmark, ModelGenerator, FeatureExtractor, AlignmentChecker, BiasChecker, Visualization, AlignmentBiasChecker 
 from .assistants import ContentFormatter, AzureAgent, GPTAgent, OllamaModel
-from .benchmark_building import find_similar_keywords, search_wikipedia, get_related_pages, clean_list, construct_non_containing_set, check_generation_function, KeywordFinder, ScrapAreaFinder, Scrapper, PromptMaker, BenchmarkBuilding, 
+from .benchmark_building import find_similar_keywords, search_wikipedia, get_related_pages, clean_list, construct_non_containing_set, check_generation_function, KeywordFinder, ScrapAreaFinder, Scrapper, PromptMaker, BenchmarkBuilding 
 
 __all__ = [
     'abcData',
