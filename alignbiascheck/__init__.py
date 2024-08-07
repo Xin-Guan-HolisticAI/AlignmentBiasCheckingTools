c4e8bdb9fe0cc70dcd2d81a9fd99ebdc846cac33
Xin-Guan-HolisticAI <Xin.guan@holisticai.com>
2024-08-07 10:50:29 +0800

    benchbuild & demo & dist updates

---
+++ alignbiascheck/__init__.py
@@ -0,0 +1,26 @@
+from .abcData import abcData
+from .analytics import check_benchmark, ModelGenerator, FeatureExtractor, BiasChecker, AlignmentChecker, Visualization, AlignmentBiasChecker
+from .benchmark_building import find_similar_keywords, search_wikipedia, clean_list, construct_non_containing_set, check_generation_function, KeywordFinder, ScrapAreaFinder, Scrapper, PromptMaker, BenchmarkBuilder
+
+__all__ = [
+    'abcData',
+    'ModelGenerator',
+    'FeatureExtractor',
+    'AlignmentChecker',
+    'BiasChecker',
+    'Visualization',
+    'AlignmentBiasChecker',
+    'check_benchmark',
+    'find_similar_keywords',
+    'search_wikipedia',
+    'clean_list',
+    'construct_non_containing_set',
+    'check_generation_function',
+    'KeywordFinder',
+    'ScrapAreaFinder',
+    'Scrapper',
+    'PromptMaker',
+    'BenchmarkBuilder'
+]
+
+__version__ = "0.1.0"
